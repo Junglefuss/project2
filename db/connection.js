8d6a2fe7d4374dc1e2cb3bcf34dab3117441c3dc
Jonathan Goldfuss <Jonathan@Jonathans-Air-3.fios-router.home>
2019-10-08 11:27:40 -0400

    deploying sign up app

--- db/connection.js
+++ db/connection.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 mongoose
-  .connect('mongoURI', { useNewUrlParser: true })
+  .connect(mongoURI, { useNewUrlParser: true })
   .then(instance => console.log(`Connected to db: ${instance.connections[0].name}`))
   .catch(error => console.log('Connection failed!', error));
 
